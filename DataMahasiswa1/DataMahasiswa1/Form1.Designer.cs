f3ccd906f96771d65fab9d0739291ab249c87588
marshaerh <76280295+marshaerh@users.noreply.github.com>
2022-03-28 10:47:37 +0700

    Add files via upload

---
+++ DataMahasiswa1/DataMahasiswa1/Form1.Designer.cs
@@ -0,0 +1,411 @@
+﻿namespace DataMahasiswa1
+{
+    partial class FrmDataKursus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbMskn = new System.Windows.Forms.ComboBox();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.dtpTanggalLahir = new System.Windows.Forms.DateTimePicker();
+            this.txtTempatLahir = new System.Windows.Forms.TextBox();
+            this.rdbPerempuan = new System.Windows.Forms.RadioButton();
+            this.rdbLaki = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvDataKursus = new System.Windows.Forms.DataGridView();
+            this.No = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnKeluar = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataKursus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(101, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(368, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "DATA KURSUS MEMASAK";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbMskn);
+            this.groupBox1.Controls.Add(this.txtAlamat);
+            this.groupBox1.Controls.Add(this.dtpTanggalLahir);
+            this.groupBox1.Controls.Add(this.txtTempatLahir);
+            this.groupBox1.Controls.Add(this.rdbPerempuan);
+            this.groupBox1.Controls.Add(this.rdbLaki);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtNama);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(16, 105);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(393, 174);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // cmbMskn
+            // 
+            this.cmbMskn.FormattingEnabled = true;
+            this.cmbMskn.Items.AddRange(new object[] {
+            "Spesialis Masakan Tradisional",
+            "Spesialis Masakan Modern",
+            "Spesialis Cake",
+            "Spesialis Kue Tart",
+            "Spesialis Kue Tradisional"});
+            this.cmbMskn.Location = new System.Drawing.Point(111, 135);
+            this.cmbMskn.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbMskn.Name = "cmbMskn";
+            this.cmbMskn.Size = new System.Drawing.Size(175, 21);
+            this.cmbMskn.TabIndex = 14;
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(111, 111);
+            this.txtAlamat.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(266, 20);
+            this.txtAlamat.TabIndex = 13;
+            // 
+            // dtpTanggalLahir
+            // 
+            this.dtpTanggalLahir.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpTanggalLahir.Location = new System.Drawing.Point(111, 87);
+            this.dtpTanggalLahir.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpTanggalLahir.Name = "dtpTanggalLahir";
+            this.dtpTanggalLahir.Size = new System.Drawing.Size(88, 20);
+            this.dtpTanggalLahir.TabIndex = 12;
+            // 
+            // txtTempatLahir
+            // 
+            this.txtTempatLahir.Location = new System.Drawing.Point(111, 63);
+            this.txtTempatLahir.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTempatLahir.Name = "txtTempatLahir";
+            this.txtTempatLahir.Size = new System.Drawing.Size(136, 20);
+            this.txtTempatLahir.TabIndex = 11;
+            // 
+            // rdbPerempuan
+            // 
+            this.rdbPerempuan.AutoSize = true;
+            this.rdbPerempuan.Location = new System.Drawing.Point(211, 42);
+            this.rdbPerempuan.Margin = new System.Windows.Forms.Padding(2);
+            this.rdbPerempuan.Name = "rdbPerempuan";
+            this.rdbPerempuan.Size = new System.Drawing.Size(79, 17);
+            this.rdbPerempuan.TabIndex = 10;
+            this.rdbPerempuan.TabStop = true;
+            this.rdbPerempuan.Text = "Perempuan";
+            this.rdbPerempuan.UseVisualStyleBackColor = true;
+            // 
+            // rdbLaki
+            // 
+            this.rdbLaki.AutoSize = true;
+            this.rdbLaki.Location = new System.Drawing.Point(111, 42);
+            this.rdbLaki.Margin = new System.Windows.Forms.Padding(2);
+            this.rdbLaki.Name = "rdbLaki";
+            this.rdbLaki.Size = new System.Drawing.Size(64, 17);
+            this.rdbLaki.TabIndex = 9;
+            this.rdbLaki.TabStop = true;
+            this.rdbLaki.Text = "Laki-laki";
+            this.rdbLaki.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(4, 143);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Jenis Masakan";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 114);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Alamat";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 44);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Jenis Kelamin";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 66);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Tempat Lahir";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 93);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tanggal Lahir";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(111, 18);
+            this.txtNama.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(208, 20);
+            this.txtNama.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 21);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nama";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvDataKursus);
+            this.groupBox2.Location = new System.Drawing.Point(16, 311);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(554, 174);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // dgvDataKursus
+            // 
+            this.dgvDataKursus.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDataKursus.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.No,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dgvDataKursus.Location = new System.Drawing.Point(0, 9);
+            this.dgvDataKursus.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvDataKursus.Name = "dgvDataKursus";
+            this.dgvDataKursus.RowHeadersWidth = 51;
+            this.dgvDataKursus.RowTemplate.Height = 24;
+            this.dgvDataKursus.Size = new System.Drawing.Size(788, 122);
+            this.dgvDataKursus.TabIndex = 5;
+            // 
+            // No
+            // 
+            this.No.HeaderText = "NO";
+            this.No.MinimumWidth = 6;
+            this.No.Name = "No";
+            this.No.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nama";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Jenis Kelamin";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Tempat Lahir";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Tanggal Lahir";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Alamat";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 125;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Jenis Masakan";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 125;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(36, 283);
+            this.btnSimpan.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(82, 24);
+            this.btnSimpan.TabIndex = 5;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(133, 283);
+            this.btnHapus.Margin = new System.Windows.Forms.Padding(2);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(82, 24);
+            this.btnHapus.TabIndex = 6;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnKeluar
+            // 
+            this.btnKeluar.Location = new System.Drawing.Point(1147, 427);
+            this.btnKeluar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnKeluar.Name = "btnKeluar";
+            this.btnKeluar.Size = new System.Drawing.Size(82, 24);
+            this.btnKeluar.TabIndex = 7;
+            this.btnKeluar.Text = "Keluar";
+            this.btnKeluar.UseVisualStyleBackColor = true;
+            this.btnKeluar.Click += new System.EventHandler(this.btnKeluar_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::DataMahasiswa1.Properties.Resources.compose_512;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(9, 10);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(75, 81);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // FrmDataKursus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 495);
+            this.Controls.Add(this.btnKeluar);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FrmDataKursus";
+            this.Text = "Form Data Kursus Memasak";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataKursus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbMskn;
+        private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.DateTimePicker dtpTanggalLahir;
+        private System.Windows.Forms.TextBox txtTempatLahir;
+        private System.Windows.Forms.RadioButton rdbPerempuan;
+        private System.Windows.Forms.RadioButton rdbLaki;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvDataKursus;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnKeluar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn No;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+    }
+}
+
---
+++ DataMahasiswa1/DataMahasiswa1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataMahasiswa1
+{
+    public partial class FrmDataKursus : Form
+    {
+        public FrmDataKursus()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            int baris = 0;
+            dgvDataKursus.Rows.Add();
+            baris = dgvDataKursus.Rows.Count - 2;
+
+            string jk;
+            if (rdbLaki.Checked == true)
+            {
+                jk = "Laki-laki";
+            }
+            else
+            {
+                jk = "Perempuan";
+            }
+
+            dgvDataKursus[0, baris].Value = dgvDataKursus.Rows.Count - 1;
+            dgvDataKursus[1, baris].Value = txtNama.Text;
+            dgvDataKursus[2, baris].Value = jk;
+            dgvDataKursus[3, baris].Value = txtTempatLahir.Text;
+            dgvDataKursus[4, baris].Value = dtpTanggalLahir.Text;
+            dgvDataKursus[5, baris].Value = txtAlamat.Text;
+            dgvDataKursus[6, baris].Value = cmbMskn.SelectedItem.ToString();
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            if (this.dgvDataKursus.SelectedRows.Count > 0)
+            {
+                dgvDataKursus.Rows.RemoveAt(this.dgvDataKursus.SelectedRows[0].Index);
+            }
+        }
+
+        private void btnKeluar_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
